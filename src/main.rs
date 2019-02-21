b0444013eaf33b95040720ff585f22845180f7d3
PJ Kim <pj.kim@kakaocorp.com>
2019-02-21 15:29:10 +0900

    Refactor code based on https://codereview.stackexchange.com/a/213897/73051

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
-use std::io::{self, prelude::*};
+use std::io::{self, Write};
+use std::num::ParseIntError;
 
 // Given a list of integers,
 // use a vector and return the mean (the average value),
@@ -8,54 +9,83 @@ use std::io::{self, prelude::*};
 // a hash map will be helpful here) of the list.
 fn main() {
     let stdin = io::stdin();
+    let mut stdout = io::stdout();
 
-    'mainloop: loop {
+    let mut line = String::new();
+
+    loop {
         print!("Enter space-separated numbers (q for quit)> ");
-        io::stdout().flush().expect("flush failed");
+        stdout.flush().unwrap();
 
-        let mut line = String::new();
-        stdin.read_line(&mut line).expect("Failed to read line");
+        line.clear();
+        stdin.read_line(&mut line).unwrap();
 
         if line == "q\n" {
-            break 'mainloop;
+            break;
         }
 
-        // TODO: handling ParseIntError
-        let nums: Vec<i32> = line
+        let mut nums = match line
             .split_whitespace()
-            .map(|s| s.parse::<i32>().unwrap())
-            .collect();
+            .map(|s| s.parse::<i32>())
+            .collect::<Result<Vec<i32>, ParseIntError>>()
+        {
+            Ok(vec) => {
+                if vec.is_empty() {
+                    continue;
+                }
+                vec
+            }
+            Err(_) => {
+                println!("Invalid input. Only integer values are accepted. Please try again.");
+                continue;
+            }
+        };
 
         let mean = nums.iter().sum::<i32>() as f32 / nums.len() as f32;
+        let median = get_median(&mut nums);
+        let mode = get_mode(&nums);
 
         println!(
-            "numbers: {:?}, mean: {}, median: {}, mode: {}",
-            nums,
-            mean,
-            get_median(&nums),
-            get_mode(&nums)
+            "numbers: {:?}, mean: {}, median: {}, mode: {:?}",
+            &nums, mean, median, mode,
         );
     }
 }
 
-fn get_median(v: &Vec<i32>) -> f32 {
-    if v.len() < 1 {
+fn get_median(vec: &mut Vec<i32>) -> f32 {
+    if vec.is_empty() {
         return 0.0;
     }
 
-    let mut vec = v.clone();
     vec.sort();
+
+    let mid_idx = vec.len() / 2;
     if vec.len() % 2 == 1 {
-        return *vec.get(vec.len() / 2).unwrap() as f32;
+        vec[mid_idx] as f32
+    } else {
+        (vec[mid_idx - 1] + vec[mid_idx]) as f32 / 2.0
     }
-    return (*vec.get(vec.len() / 2 - 1).unwrap() + *vec.get(vec.len() / 2).unwrap()) as f32 / 2.0;
 }
 
-fn get_mode(v: &Vec<i32>) -> i32 {
-    let mut map = HashMap::new();
-    for num in v {
+fn get_mode(slice: &[i32]) -> Vec<i32> {
+    if slice.is_empty() {
+        return vec![];
+    }
+
+    let mut map = HashMap::with_capacity(slice.len());
+    for num in slice {
         let count = map.entry(num).or_insert(0);
         *count += 1;
     }
-    return **map.iter().max_by_key(|(_, v)| *v).unwrap().0;
+
+    let max_val = map.values().map(|v| *v).max().unwrap();
+
+    let mut vec = map
+        .into_iter()
+        .filter_map(|(k, v)| if v == max_val { Some(*k) } else { None })
+        .collect::<Vec<i32>>();
+
+    vec.sort();
+
+    vec
 }
